b8fcdcd10381b2fda3389b62dbe2dd4a85911a1d
Pierre Vermeil <Pierre Vermeil>
2024-02-08 01:40:13 +0100

    mix app initial commit

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\String\u;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        return new Response('Vinyl to play music');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = u(str_replace('-', '', $slug))->title(true);
+        } else {
+            $title = 'All genres';
+        }
+
+        return new Response($title);
+    }
+}
